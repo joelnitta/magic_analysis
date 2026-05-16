e6a1febd5f0b765fe6e18c2bd6a63b2de8841d9b
joelnitta <joelnitta@gmail.com>
2026-05-16 17:15:11 +0900

    Add workflows to deploy to shinyapps.io

---
+++ deploy.R
@@ -0,0 +1,35 @@
+# Deploy Shiny app to shinyapps.io
+#
+# Usage:
+# 1. Set up your shinyapps.io account token (see below).
+# 2. Run this script in the R console after running targets::tar_make().
+#
+# This will deploy app.R and the required data files to shinyapps.io.
+
+# ---- Setup (run once per machine) ----
+# Uncomment and fill in your shinyapps.io account info the first time you deploy.
+# DO NOT COMMIT CREDENTIALS TO GIT
+# library(rsconnect)
+# rsconnect::setAccountInfo(
+#   name = "<your-account-name>",
+#   token = "<your-token>",
+#   secret = "<your-secret>"
+# )
+
+# ---- Deploy app ----
+library(rsconnect)
+
+# List of files required for the app to run
+app_files <- c(
+  "app.R",
+  "working/wins_losses.csv",
+  "working/deck_color_counts.csv",
+  "working/lands_set_averages.csv"
+)
+
+# Deploy the app (change appName as desired)
+rsconnect::deployApp(
+  appDir = ".",
+  appFiles = app_files,
+  appName = "magic-dashboard"
+)
